a12d552768258e60e4eb77f4fd2301e1a9dac0d4
Dede Suwandy <dedesuwandy45@gmail.com>
2020-12-13 22:41:48 +0700

    Membuat Presensi Keluar

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PresensiController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 route::get('/registrasi',[LoginController::class,'registrasi'])->name('registrasi');
 route::post('/simpanregistrasi',[LoginController::class,'simpanregistrasi'])->name('simpanregistrasi');
@@ -16,10 +19,12 @@ route::post('/postlogin',[LoginController::class,'postlogin'])->name('postlogin'
 route::get('/Logout',[LoginController::class,'logout'])->name('Logout');
 
 Route::group(['middleware' => ['auth','ceklevel:admin,karyawan']], function () {
-    route::get('/Home',[Homecontroller::class,'index'])->name('Home');
+    route::get('/Home',[HomeController::class,'index'])->name('Home');    
 });
 
 Route::group(['middleware' => ['auth','ceklevel:karyawan']], function () {
     route::post('/simpan-masuk',[PresensiController::class,'store'])->name('simpan-masuk');
-    route::get('/presensi-masuk',[PresensiController::class,'index'])->name('presensi-masuk');
+    route::get('/presensi-masuk',[PresensiController::class,'index'])->name('presensi-masuk');    
+    route::get('/presensi-keluar',[PresensiController::class,'keluar'])->name('presensi-keluar');   
+    Route::post('/ubah-presensi',[PresensiController::class,'presensipulang'])->name('ubah-presensi');
 });
